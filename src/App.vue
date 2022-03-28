b1c8f2fc429840142c025bc0cf6c02f2d7494238
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-03-28 17:56:51 +0900

    add bottom navigation library

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ShuttleHome />
+    <GrowBottomNavigation :options="options"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { GrowBottomNavigation } from "bottom-navigation-vue"
+import ShuttleHome from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ShuttleHome, GrowBottomNavigation,
+  },
+  data: () => ({
+    selected: 1,
+    options: [
+      { id: 1, icon: "fas fa-home", title: "Home", badge: 1 },
+      { id: 2, icon: "fas fa-layer-group", title: "Category" },
+      { id: 3, icon: "fas fa-cog", title: "Setting" },
+      { id: 4, icon: "fas fa-user", title: "Account", badge: 15 },
+      { id: 5, icon: "fas fa-bell", title: "Notification" },
+    ],
+  }),
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+  white-space: nowrap;
 }
 </style>
