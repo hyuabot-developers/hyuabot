8a2b3b5c21068851b6ed90c2591c39a32832547d
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-03-29 18:28:13 +0900

    feat: 하단 네비게이션 추가

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-bottom-navigation :value="value" color="teal" grow fixed height="200px" width="100%">
+      <v-btn text height="100%">Recent<v-icon>mdi-history</v-icon></v-btn>
+      <v-btn text height="100%">Favorites<v-icon>mdi-heart</v-icon></v-btn>
+      <v-btn text height="100%">Nearby<v-icon>mdi-map-marker</v-icon></v-btn>
+      <v-btn text height="100%">Favorites<v-icon>mdi-heart</v-icon></v-btn>
+      <v-btn text height="100%">Nearby<v-icon>mdi-map-marker</v-icon></v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+
+  data: () => ({
+    value: 0
+  }),
+};
+</script>
