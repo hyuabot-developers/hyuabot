7235efa3a70beca42442ca72d770ec02dd7b1019
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-04-05 18:10:03 +0900

    feat: axios에 대한 BASE_URL 설정

--- src/views/ShuttlePage.vue
+++ src/views/ShuttlePage.vue
@@ -18,6 +18,7 @@ ShuttleCard {
 <script>
 import ShuttleCard from "@/components/ShuttleCard";
 import axios from "axios";
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 export default {
   name: "ShuttlePage",
   components: { ShuttleCard },
