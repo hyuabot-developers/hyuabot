7359b8157ba548f01a36484239377024792c22a4
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-04-12 11:22:01 +0900

    feat:  Workbox를 이용한 성능 향상

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,36 +1,19 @@
-/* eslint-disable no-console */
+import { Workbox } from "workbox-window";
+export default function registerServiceWorker() {
+  if ("production" !== process.env.NODE_ENV) {
+    return;
+  }
 
-import { register } from "register-service-worker";
-
-if (process.env.NODE_ENV === "production") {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        "App is being served from cache by a service worker.\n" +
-          "For more details, visit https://goo.gl/AFskqB"
-      );
-    },
-    registered() {
-      console.log("Service worker has been registered.");
-    },
-    cached() {
-      console.log("Content has been cached for offline use.");
-    },
-    updatefound() {
-      console.log("New content is downloading.");
-    },
-    updated() {
-      console.log("New content is available; please refresh.");
-    },
-    offline() {
-      console.log(
-        "No internet connection found. App is running in offline mode."
-      );
-    },
-    error(error) {
-      console.error("Error during service worker registration:", error);
-    },
-  });
+  if ("serviceWorker" in navigator) {
+    const wb = new Workbox("/service-worker.js");
+    wb.addEventListener("installed", (event) => {
+      if (
+        event.isUpdate &&
+        confirm("New content is available; please refresh.")
+      ) {
+        window.location.reload();
+      }
+    });
+    wb.register().then(() => console.log("service worker registered"));
+  }
 }
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
