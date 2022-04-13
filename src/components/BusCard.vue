c0caac0a395e8da7452691a5063fa2a8fe154de0
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-04-13 16:11:44 +0900

    feat: 버스 도착 정보 출력할 수 있도록 수정

--- src/components/BusCard.vue
+++ src/components/BusCard.vue
@@ -1,35 +1,100 @@
 <template>
-  <v-card>
-    <v-container pa-1>
-      <v-layout row>
-        <v-flex xs7>
-          <v-card-title primary-title>
-            <div>
-              <div class="grey--text">전체 사용자</div>
-              <h3 class="headline">123</h3>
-            </div>
-          </v-card-title>
-        </v-flex>
-        <v-flex xs5>
-          <v-card-title primary-title>
-            <v-avatar>
-              <v-icon medium color="success">person</v-icon>
-            </v-avatar>
-          </v-card-title>
-        </v-flex>
-      </v-layout>
-      <v-divider light></v-divider>
-      <v-card-actions class="pa-2">
-        <v-icon small color="primary">update</v-icon>
-        <span class="grey--text caption font-italic">&nbsp;5분전</span>
-        <v-spacer></v-spacer>
-      </v-card-actions>
+  <v-card width="95vw" height="125">
+    <v-container>
+      <v-row
+        no-gutters
+        justify="center"
+        style="height: 40px; font-size: 1.25em"
+        v-bind:style="{ color: getLineColor(item.lineName) }"
+      >
+        {{ item.lineName }}
+      </v-row>
+      <v-divider />
+      <v-row no-gutters style="height: 75px" align="center" justify="center">
+        <v-col cols="4">
+          <li
+            v-for="(realtimeItem, index) in item.arrivalList.slice(
+              0,
+              Math.min(2, item.arrivalList.length)
+            )"
+            :key="index"
+            style="margin-bottom: 20px"
+            v-bind:style="{
+              color: getCurrentStationColor(realtimeItem['currentStation']),
+            }"
+          >
+            {{ parseInt(realtimeItem["remainedTime"]) }}분<br />
+          </li>
+        </v-col>
+        <v-col cols="4">
+          <li
+            v-for="(realtimeItem, index) in item.arrivalList.slice(
+              0,
+              Math.min(2, item.arrivalList.length)
+            )"
+            :key="index"
+            style="margin-bottom: 20px"
+          >
+            {{
+              realtimeItem.location > 0
+                ? `${realtimeItem["location"]} 전`
+                : "출발지 대기중"
+            }}<br />
+          </li>
+        </v-col>
+        <v-col cols="4">
+          <li
+            v-for="(realtimeItem, index) in item.arrivalList.slice(
+              0,
+              Math.min(2, item.arrivalList.length)
+            )"
+            :key="index"
+            style="margin-bottom: 20px"
+          >
+            {{
+              realtimeItem.remainedSeat > 0
+                ? `${realtimeItem["remainedSeat"]} 석`
+                : ""
+            }}<br />
+          </li>
+        </v-col>
+      </v-row>
     </v-container>
   </v-card>
 </template>
-<style scoped></style>
+<style scoped>
+v-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+}
+li {
+  display: inline;
+  list-style-type: none;
+}
+</style>
 <script>
 export default {
-  name: "BusCard",
+  name: "SubwayCard",
+  methods: {
+    getLineColor: function (lineName) {
+      console.log(lineName);
+      switch (lineName) {
+        case "10-1":
+          return "#33CC99";
+        default:
+          return "#eE0012";
+      }
+    },
+    getCurrentStationColor: function (currentStation) {
+      return currentStation === "시간표" ? "#7f7f7f" : "#000000";
+    },
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
