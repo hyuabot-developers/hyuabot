e19b1b514a5f40bb86de36fd011e8d2fc1487c02
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-04-25 18:15:27 +0900

    feat: PWA manifest 갱신

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -19,7 +19,7 @@ const routes: RouteRecordRaw[] = [
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    redirect: 'shuttle',
   },
 ];
 
