134f34c7444eec8b9d5d9274d28714c6fbc5a3d8
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-04-28 16:25:21 +0900

    fix: Change icon theme on menu page

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -3,31 +3,31 @@
     <q-list bordered separator style="width: 100%; height: 240px; margin: 10px">
       <q-item clickable>
         <q-item-section avatar v-ripple>
-          <q-icon color="secondary" name="mdi-library" />
+          <q-icon style="color: var(--q-primary)" name="mdi-library" />
         </q-item-section>
         <q-item-section>열람실 좌석 정보</q-item-section>
       </q-item>
       <q-item clickable>
         <q-item-section avatar v-ripple>
-          <q-icon color="secondary" name="mdi-calendar" />
+          <q-icon style="color: var(--q-primary)" name="mdi-calendar" />
         </q-item-section>
         <q-item-section>학사력</q-item-section>
       </q-item>
       <q-item clickable>
         <q-item-section avatar v-ripple>
-          <q-icon color="secondary" name="mdi-map" />
+          <q-icon style="color: var(--q-primary)" name="mdi-map" />
         </q-item-section>
         <q-item-section>캠퍼스 지도</q-item-section>
       </q-item>
       <q-item clickable>
         <q-item-section avatar v-ripple>
-          <q-icon color="secondary" name="mdi-account-box" />
+          <q-icon style="color: var(--q-primary)" name="mdi-account-box" />
         </q-item-section>
         <q-item-section>전화부</q-item-section>
       </q-item>
       <q-item clickable>
         <q-item-section avatar v-ripple>
-          <q-icon color="secondary" name="mdi-cog" />
+          <q-icon style="color: var(--q-primary)" name="mdi-cog" />
         </q-item-section>
         <q-item-section>설정</q-item-section>
       </q-item>
