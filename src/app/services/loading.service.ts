1c97630389cde0dd20acce2ffa515af68e8c60fa
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-10-09 15:26:54 +0900

    fix: 로딩 화면이 제대로 나오지 않는 문제 수정

--- src/app/services/loading.service.ts
+++ src/app/services/loading.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({ providedIn: 'root' })
 export class LoadingService {
-  constructor(public loadingController: LoadingController) {}
+  constructor(public loadingController: LoadingController, private translateService: TranslateService) {}
   async present(loadingID: string, loadingMessage: string) {
     const loading = await this.loadingController.create({
       id: loadingID,
-      message: loadingMessage,
+      message: this.translateService.instant(loadingMessage),
       spinner: 'crescent'
     });
     await loading.present();
