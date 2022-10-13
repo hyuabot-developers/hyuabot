1ca5cc542ded258b32b48e5f1d75781a3642099d
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-10-13 18:20:38 +0900

    feat: 버스 실시간 페이지 구성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { shuttleServiceInjectables } from './pages/shuttle/shuttle.service';
 import { shuttleTimetableServiceInjectables } from './pages/shuttle/timetable/shuttle-timetable.service';
+import { busServiceInjectables } from './pages/bus/bus.service';
 
 export const createTranslateLoader = (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
@@ -22,7 +23,7 @@ export const createTranslateLoader = (http: HttpClient) => new TranslateHttpLoad
   }), GraphQLModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    shuttleServiceInjectables, shuttleTimetableServiceInjectables
+    shuttleServiceInjectables, shuttleTimetableServiceInjectables, busServiceInjectables
   ],
   bootstrap: [AppComponent],
 })
