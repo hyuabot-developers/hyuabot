59ab9c69de693f6f35fe15a0ee77ddd916cdf546
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-10-27 15:03:16 +0900

    remove web directory

--- web/src/models/readingRoom.item.ts
+++
@@ -1,9 +0,0 @@
-export interface ReadingRoomItem {
-  name: string;
-  isActive: boolean;
-  isReserved: boolean;
-  total: number;
-  activeTotal: number;
-  occupied: number;
-  available: number;
-}
